94d85ada967d4024234a43e989b91b951d65f2a7
Victor Nordstrand <victor.nordstrand@gmail.com>
2021-01-14 01:35:05 +0100

    add todo-api

---
+++ todo-api/src/TodoWebApi/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TodoWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
